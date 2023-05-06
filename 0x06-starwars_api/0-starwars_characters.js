6d614b69f9c3f472fabb4da1c8666113b599a002
Jason Flair <emelieobumse100@gmail.com>
2023-05-06 13:36:35 +0100

    all done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,23 +2,23 @@
 
 const request = require('request');
 const movieId = process.argv[2];
-const mainUrl = 'https://swapi-api.alx-tools.com/api/';
+const mainUrl = 'https://swapi-api.alx-tools.com/api/films/';
 
-const requestUrl = mainUrl + 'films/' + movieId;
+const requestUrl = mainUrl + movieId;
 
 request(requestUrl, (error, response, body) => {
   if (error) {
     console.log(error);
   }
-  resp = JSON.parse(body);
-  characters = resp.characters;
-  for (character of characters) {
+  const resp = JSON.parse(body);
+  const characters = resp.characters;
+  for (const character of characters) {
     request(character, (error, response, body) => {
       if (error) {
         console.log(error);
       }
-      nameResponse = JSON.parse(body);
-      console.log(nameResponse.name)
-    })
+      const nameResponse = JSON.parse(body);
+      console.log(nameResponse.name);
+    });
   }
 });
