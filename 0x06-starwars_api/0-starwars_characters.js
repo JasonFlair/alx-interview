2d666bab75e638b57261d1c9daf71c1af6ce19fb
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2023-05-06 12:31:28 +0000

    all done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,12 +2,23 @@
 
 const request = require('request');
 const movieId = process.argv[2];
-const mainUrl = 'https://swapi-api.alx-tools.com/api/films/';
+const mainUrl = 'https://swapi-api.alx-tools.com/api/';
 
-const requestUrl = mainUrl + movieId;
+const requestUrl = mainUrl + 'films/' + movieId;
 
-const resp = request(requestUrl, (error, response, body) => {
-  if (error) console.log(error);
-  return body;
+request(requestUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  resp = JSON.parse(body);
+  characters = resp.characters;
+  for (character of characters) {
+    request(character, (error, response, body) => {
+      if (error) {
+        console.log(error);
+      }
+      nameResponse = JSON.parse(body);
+      console.log(nameResponse.name)
+    })
+  }
 });
-console.log(resp);
