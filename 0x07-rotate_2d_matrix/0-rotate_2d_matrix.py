f6ee241f48a49c290205f652edfa054f7eb96b92
Jason Flair <emelieobumse100@gmail.com>
2023-05-12 11:09:24 +0100

    rotate 2d matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-'''2D matrix'''
+"""rotate a 2d matrix"""
 
 
 def rotate_2d_matrix(matrix):
-    '''rotates a 2d matrix 90° clockwise
-    Returns: Nothing'''
-    left, right = 0, len(matrix) - 1
+    """function to do the job"""
+    # temp array that will transfer its
+    # content to the original matrix
+    temp_list = []
+    # temp array that will push to temp_list
+    temp_temp_list = []
+    n = len(matrix)
+    for i in range(n):
+        for j in range(n):
+            temp_temp_list.insert(0, matrix[j][i])
+        temp_list.append(temp_temp_list)
+        temp_temp_list = []
 
-    while left < right:
-        for i in range(right - left):
-            top, bottom = left, right
-            # save topleft  value
-            topLeft = matrix[top][left + i]
-            # move bottom left to top left
-            matrix[top][left + i] = matrix[bottom - i][left]
-            # move bottom right to bottom left
-            matrix[bottom - i][left] = matrix[bottom][right - i]
-            # move top right to bottom right
-            matrix[bottom][right - i] = matrix[top + i][right]
-            # move top left to top right
-            matrix[top + i][right] = topLeft
-        right -= 1
-        left += 1
+    # after temp list operation is completed,
+    # it will be the matrix in 90 degrees
+    # so remove everything in the matrix and append temp list content
+    del matrix[:]
+    for i in temp_list:
+        matrix.append(i)
