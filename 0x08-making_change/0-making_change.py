035a6efdf8580471cce2cf315b3e1823bf692125
Jason Flair <emelieobumse100@gmail.com>
2023-05-20 17:50:19 +0100

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
-'''Given a pile of coins of different values,
+"""Given a pile of coins of different values,
     determine the fewest number of coins needed to meet
     a given amount total.
-'''
-import sys
+"""
 
 
 def makeChange(coins, total):
-    '''
+    """
     Return: fewest number of coins needed to meet total
     If total is 0 or less, return 0
     If total cannot be met by any number of coins you have, return -1
-    '''
-    if total <= 0:
+    """
+    n = 0
+    fewest_num = 1
+    sorted_coins = sorted(coins, reverse=True)
+    placeholder_total = sorted_coins[n]
+    if total == 0 or total < 0:
         return 0
-    table = [sys.maxsize for i in range(total + 1)]
-    table[0] = 0
-    m = len(coins)
-    for i in range(1, total + 1):
-        for j in range(m):
-            if coins[j] <= i:
-                subres = table[i - coins[j]]
-                if subres != sys.maxsize and subres + 1 < table[i]:
-                    table[i] = subres + 1
-
-    if table[total] == sys.maxsize:
+    try:
+        while True:
+            placeholder_total += sorted_coins[n]
+            if placeholder_total > total:
+                # remove the just added coin value
+                placeholder_total -= sorted_coins[n]
+                n += 1
+            else:
+                fewest_num += 1
+            if placeholder_total == total:
+                return fewest_num
+    except IndexError:
+        # if the movement of n has gone beyond the last coin value
         return -1
-    return table[total]
